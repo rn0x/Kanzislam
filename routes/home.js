c336769ab84b921831618b0c95825547bfae1c04
rn0x <ry2n1996@gmail.com>
2023-10-01 08:51:22 +0300

    variables head

--- routes/home.js
+++ routes/home.js
@@ -2,10 +2,13 @@ export default async (param) => {
 
     param.app.get('/', async (request, response) => {
 
-        console.log(param.config);
+        const config = param.config;
         let options = {
-            title: "عنوان الصفحة",
-            test: "تجريبي تجريبي"
+            website_name: config.website_name,
+            title: `عنوان الصفحة - ${config.website_name}`,
+            keywords: ["word1", "word2", "word3"],
+            description: "وصف_الصفحة",
+            preview: "صورة_المعاينة_للصفحة",
         };
         let pugPath = param.path.join(param.__dirname, './views/home.pug');
         let render = param.pug.renderFile(pugPath, { options: options, jsStringify: param.jsStringify });
