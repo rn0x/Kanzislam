171ba886f845a6cb54774fd38bf7e85979126204
rn0x <ry2n1996@gmail.com>
2023-10-10 00:31:17 +0300

    تحسـيـنـات القائمة الجانبية

--- public/js/main.js
+++ public/js/main.js
@@ -1,61 +1,74 @@
 document.addEventListener("DOMContentLoaded", async function () {
-
     const options = window.options;
-    let header_menu_left = document.getElementById("header_menu_left");
-    let header_menu_left_isLoggedIn = document.getElementById("header_menu_left_isLoggedIn");
-    let menu_logged = document.getElementById("menu_logged");
-    let isMenu = false
+    const header_menu_left = document.getElementById("header_menu_left");
+    const header_menu_left_isLoggedIn = document.getElementById("header_menu_left_isLoggedIn");
+    const menu_logged = document.getElementById("menu_logged");
+    let isMenu = false;
+    let isSideMenu = false;
 
-    // احصل على زر القائمة
     const menuButton = document.getElementById('button_menu_header');
-    // احصل على القائمة الجانبية
     const sideMenuBox = document.getElementById('SideMenuBox');
     const sideMenu = document.getElementById('SideMenu');
 
-    // أضف استماع للنقر على زر القائمة
-    menuButton.addEventListener('click', function () {
-        sideMenuBox.style.display = "block";
-        sideMenuBox.classList.toggle('active');
-        sideMenu.classList.toggle('active');
-    });
+    menuButton.addEventListener('click', toggleSideMenu);
 
+    document.addEventListener('click', closeSideMenu);
 
     if (options?.session?.isLoggedIn) {
-
         header_menu_left.style.display = "none";
         header_menu_left_isLoggedIn.style.display = "block";
-
-        header_menu_left_isLoggedIn.addEventListener("click", async () => {
-
-            let logoutButton = document.getElementById("logoutButton");
-
-            if (isMenu === false) {
-                menu_logged.style.display = "block";
-                isMenu = true;
-
-                logoutButton.addEventListener("click", async () => {
-
-                    let logoutURL = `${window.location.origin}/logout`;
-                    let logoutFetch = await fetch(logoutURL, {
-                        method: "POST",
-                        headers: {
-                            'Content-Type': 'application/json',
-                        }
-                    });
-                    let response = await logoutFetch?.json();
-
-                    if (response?.logout) {
-                        window.location.href = "/";
-                    }
-                });
-            }
-
-            else {
-                menu_logged.style.display = "none";
-                isMenu = false
-            }
-        });
+        header_menu_left_isLoggedIn.addEventListener("click", toggleMenu);
     }
 
+    async function toggleSideMenu(event) {
+        sideMenuBox.classList.toggle('active');
+        sideMenu.classList.toggle('active');
+        if (isSideMenu) {
+            sideMenuBox.style.display = "none";
+            isSideMenu = false;
+        } else {
+            sideMenuBox.style.display = "block";
+            isSideMenu = true;
+        }
+    }
 
-});
+    async function closeSideMenu(event) {
+        const targetElement = event.target;
+        if (!targetElement.closest('#SideMenu') && !targetElement.closest('#button_menu_header') && isSideMenu) {
+            sideMenuBox.classList.toggle('active');
+            sideMenu.classList.toggle('active');
+            sideMenuBox.style.display = "none";
+            isSideMenu = false;
+        } else if (!targetElement.closest('#header_menu_left_isLoggedIn') && !targetElement.closest('#menu_logged') && isMenu) {
+            menu_logged.style.display = "none";
+            isMenu = false;
+        }
+    }
+
+    async function toggleMenu() {
+        const logoutButton = document.getElementById("logoutButton");
+        if (!isMenu) {
+            logoutButton.addEventListener("click", logout);
+            menu_logged.style.display = "block";
+            isMenu = true;
+        } else {
+            menu_logged.style.display = "none";
+            isMenu = false;
+        }
+    }
+
+    async function logout() {
+        let logoutURL = `${window.location.origin}/logout`;
+        let logoutFetch = await fetch(logoutURL, {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        });
+        let response = await logoutFetch?.json();
+
+        if (response?.logout) {
+            window.location.href = "/";
+        }
+    }
+});  
