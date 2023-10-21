4e722206f04ae1c1f0ae5e7a928b872f2f10b89c
rn0x <ry2n1996@gmail.com>
2023-10-21 13:31:31 +0300

    تحسيم القائمة الجامنبية والمنسدلة

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", async function () {
+document.addEventListener("DOMContentLoaded", function () {
     const options = window.options;
     const header_menu_left = document.getElementById("header_menu_left");
     const header_menu_left_isLoggedIn = document.getElementById("header_menu_left_isLoggedIn");
@@ -9,18 +9,42 @@ document.addEventListener("DOMContentLoaded", async function () {
     const menuButton = document.getElementById('button_menu_header');
     const sideMenuBox = document.getElementById('SideMenuBox');
     const sideMenu = document.getElementById('SideMenu');
+    const SideMenuLogin = document.getElementById('SideMenuLogin');
+    const SideMenuRegister = document.getElementById('SideMenuRegister');
+    const SideMenuProfile = document.getElementById('SideMenuProfile');
+    const SideMenuSettings = document.getElementById('SideMenuSettings');
+    const SideMenuLogout = document.getElementById('SideMenuLogout');
+    const toggleMenuProfile = document.getElementById('toggleMenuProfile');
+    const logoutButton = document.getElementById('logoutButton');
 
+    // تبديل حالة القائمة الجانبية عند النقر على زر القائمة
     menuButton.addEventListener('click', toggleSideMenu);
-
+    // إغلاق القائمة الجانبية عند النقر خارجها
     document.addEventListener('click', closeSideMenu);
 
     if (options?.session?.isLoggedIn) {
         header_menu_left.style.display = "none";
         header_menu_left_isLoggedIn.style.display = "block";
+        SideMenuLogout.style.display = "block";
+        SideMenuLogin.style.display = "none";
+        SideMenuRegister.style.display = "none";
+        SideMenuProfile.href = `${window.location.origin}/username/${options?.session?.username}`;
+        toggleMenuProfile.href = `${window.location.origin}/username/${options?.session?.username}`;
+
+        // تبديل حالة القائمة المنسدلة عند النقر على القائمة المستخدم المسجل الدخول
         header_menu_left_isLoggedIn.addEventListener("click", toggleMenu);
+
+        // زر تسجيل الخروج في القائة الجانبية
+        SideMenuLogout.addEventListener("click", logout);
     }
 
-    async function toggleSideMenu(event) {
+    else {
+        SideMenuProfile.style.display = "none";
+        SideMenuSettings.style.display = "none";
+    }
+
+    // تبديل حالة القائمة الجانبية
+    function toggleSideMenu(event) {
         sideMenuBox.classList.toggle('active');
         sideMenu.classList.toggle('active');
         if (isSideMenu) {
@@ -32,7 +56,8 @@ document.addEventListener("DOMContentLoaded", async function () {
         }
     }
 
-    async function closeSideMenu(event) {
+    // إغلاق القائمة الجانبية
+    function closeSideMenu(event) {
         const targetElement = event.target;
         if (!targetElement.closest('#SideMenu') && !targetElement.closest('#button_menu_header') && isSideMenu) {
             sideMenuBox.classList.toggle('active');
@@ -45,7 +70,8 @@ document.addEventListener("DOMContentLoaded", async function () {
         }
     }
 
-    async function toggleMenu() {
+    // تبديل حالة القائمة المنسدلة
+    function toggleMenu() {
         const logoutButton = document.getElementById("logoutButton");
         if (!isMenu) {
             logoutButton.addEventListener("click", logout);
@@ -57,6 +83,7 @@ document.addEventListener("DOMContentLoaded", async function () {
         }
     }
 
+    // تسجيل الخروج
     async function logout() {
         let logoutURL = `${window.location.origin}/logout`;
         let logoutFetch = await fetch(logoutURL, {
@@ -71,4 +98,4 @@ document.addEventListener("DOMContentLoaded", async function () {
             window.location.href = "/";
         }
     }
-});  
+});
